66206f2d9a7b5a44cddb9ff8d345ff6722a4b87b
mailys50 <maiysbrizuela@gmail.com>
2021-05-28 07:58:23 -0400

    creacion de carpetas y archivos de prueba sin contenido, funcion leer archivo lista

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
-module.exports = () => {
-  // ...
-};
+// module.exports = () => {
+//   // ...
+// };
+//leer archivo de un directorio de forma sincrona
+const fs = require ('fs');
+//funcion asincrona recibe un collback
+const read = fs.readdir('./', (error, files) => {
+  if (error) {
+    throw error;
+  }
+  // process.stdout.write(file.toString());
+  console.log(files);
+
+//leer archivo
+var archivo = fs.readFile('./proyectos/proyectoUno.txt', 'UTF-8', (error, archivo) => {
+  // const read = fs.readdir('./', (error, files) => {
+    if (error) {
+      throw error;
+    }
+    // process.stdout.write(file.toString());
+    console.log(archivo);
+  });  
+  console.log('contenido del archivo...');
+});  
+  
+  
+
+
+    
