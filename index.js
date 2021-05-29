914a016ab4f37bab652376f513d24fe642506e8d
mailys50 <maiysbrizuela@gmail.com>
2021-05-29 13:15:59 -0400

    mejoras de la función leer archivo e implementacion de la función validar ruta

--- index.js
+++ index.js
@@ -1,29 +1,47 @@
-// module.exports = () => {
-//   // ...
-// };
+// // module.exports = () => {
+// //   // ...
+// // };
 //leer archivo de un directorio de forma sincrona
-const fs = require ('fs');
+const fs = require("fs");
 //funcion asincrona recibe un collback
-const read = fs.readdir('./', (error, files) => {
-  if (error) {
-    throw error;
-  }
-  // process.stdout.write(file.toString());
-  console.log(files);
-
-//leer archivo
-var archivo = fs.readFile('./proyectos/proyectoUno.txt', 'UTF-8', (error, archivo) => {
-  // const read = fs.readdir('./', (error, files) => {
+const fileReadDirect = (router) => {
+  fs.readdir(router, (error, files) => {
     if (error) {
       throw error;
     }
-    // process.stdout.write(file.toString());
+    console.log(files);
+  });
+};
+fileReadDirect("./");
+
+//leer archivo
+const fileRead = (router, encode) => {
+  fs.readFile(router, encode, (error, archivo) => {
+    if (error) {
+      throw error;
+    }
     console.log(archivo);
-  });  
-  console.log('contenido del archivo...');
-});  
-  
-  
+  });
+  console.log("contenido del archivo...");
+};
+
+fileRead("./proyectos/proyectoUno.txt", "UTF-8");
 
 
-    
+
+// //Para verificar si un archivo o carpeta existe utiliza el método stat del módulo fs:
+
+// var fs = require("fs");
+const validateRouter = (router) =>{
+fs.stat(router, function (err) {
+  if (err == null) {
+    console.log("ruta valida");
+  } else if (err.code == "ENOENT") {
+    console.log("la ruta no es valida");
+  } else {
+    console.log(err); // ocurrió algún error
+  }
+});
+}
+validateRouter("./proyectos/proyectoUno.txt");
+validateRouter("./proyectos/proyectoU.txt");
