5d6ab1d4677a4a2a7cb245159912c18441aada19
mailys50 <maiysbrizuela@gmail.com>
2021-06-03 21:27:48 -0400

    inicio de reestructuracion de funciones

---
+++ README1.md
@@ -0,0 +1,28 @@
+# Cifrado César
+
+## Índice
+
+* [1. Preámbulo](#1-preámbulo)
+* [2. Resumen del proyecto](#2-resumen-del-proyecto)
+* [3. Criterios de aceptación mínimos del proyecto](#3-criterios-de-aceptación-mínimos-del-proyecto)
+* [4. Criterios de aceptación adicionales](#4-criterios-de-aceptación-adicionales)
+* [5. Objetivos de aprendizaje](#5-objetivos-de-aprendizaje)
+* [6. Recomendaciones generales](#6-recomendaciones-generales)
+* [7. Recursos y temas relacionados](#7-recursos-y-temas-relacionados)
+* [8. Checklist](#8-checklist)
+
+***
+
+[leer el flujo de archivos línea por línea](https://stackabuse.com/reading-a-file-line-by-line-in-node-js)
+Como es un módulo no nativo, debemos asegurarnos de haber inicializado el proyecto npm de manera adecuada npm inity luego instalarlo:
+
+[markdown-to-html](https://www.npmjs.com/package/markdown-to-html)
+$ npm install --save line-reader
+$ npm i markdown-to-html -g
+
+Para obtener la URL actual, aprovecharemos el Locationobjeto y recuperaremos su hrefpropiedad:
+
+var url = window.location.href
+console.log(url)
+
+
--- index.js
+++ index.js
@@ -1,111 +1,139 @@
-const { rejects } = require("assert");
+
 const fs = require("fs");
 const path = require("path");
-const { resolve } = require("path");
+const readline = require('readline');
+// const marked = require('marked');
+const { inflateRawSync } = require("zlib");
 
-
-
-// // //Para verificar si un archivo o carpeta existe utiliza el método stat del módulo fs:
-const validateRouter = (router) => {
-  return new Promise((resolve, rejects) => {
-    fs.stat(router, function (err) {
-      if (err == null) {
-        resolve(true);
-      } else if (err.code == "ENOENT") {
-        resolve(false);
-      } else {
-        rejects(err); // ocurrió algún error
-      }
-    });
+//leer archivo
+const getLinks = (router) => {
+  
+  const readInterface = readline.createInterface({
+      input: fs.createReadStream(router),
+      output: process.stdout,
+      console: false
   });
-};
-
-const verifTypeRouter = (router) => {
-
-    fs.lstat(router, (err, stats) => {
-      if (stats.isFile()) {
-        return router;
-      }
-      if (stats.isDirectory()) {
-        return router;  //sincrono y directo
-      }
-      if (err) 
-      rejects(err);
-    });
-  
-};
-
- const fileRead = (router) => {
-   return new Promise((resolve, rejects) =>{
-    fs.readFile(router, 'utf-8', (error, archivo) => {
-      if (error) {
-        rejects (error);
-      }
-      resolve(archivo); //extraer md
-    });
+  readInterface.on('line', function(line) {
+    console.log(line);
+    const regexp =   '^https?:\/\/[\w\-]+(\.[\w\-]+)+[/#?]?.*$';
     
-   })
-  
-};
+    
+     return  router.matchAll(regexp);
 
-  const verifExtencion = (router) => {
-    return new Promise((resolve, rejects) => {
-        const extencion= path.extname(router);
-  if(extencion == '.md'){
-    resolve(true)
-  }else{
-    rejects(error);
-  }
-
-    })
+});
 
 }
-const detectarLinks = (arcchivo) => {
+
+
+getLinks('./README1.md');
+
+const detectarLinks = (archivo) => {
   const regexp =   '^https?:\/\/[\w\-]+(\.[\w\-]+)+[/#?]?.*$';
   return  archivo.matchAll(regexp);
 }
+console.log(detectarLinks('./README1.md'));
 
 
+// // // //Para verificar si un archivo o carpeta existe utiliza el método stat del módulo fs:
+// const validateRouter = (router) => {
+//   return new Promise((resolve, rejects) => {
+//     fs.stat(router, function (err) {
+//       if (err == null) {
+//         resolve(true);
+//       } else if (err.code == "ENOENT") {
+//         resolve(false);
+//       } else {
+//         rejects(err); // ocurrió algún error
+//       }
+//     });
+//   });
+// };
 
+// const verifTypeRouter = (router) => {
 
+//     fs.lstat(router, (err, stats) => {
+//       if (stats.isFile()) {
+//         return router;
+//       }
+//       if (stats.isDirectory()) {
+//         return router;  //sincrono y directo
+//       }
+//       if (err) 
+//       rejects(err);
+//     });
+  
+// };
 
-
-
-
-
-
-
-
-const mdLinks = (router) => {
-  validateRouter(router).then((valid) => {
-    if (valid) {
-      if (path.isAbsolute(router)) {
-        verifTypeRouter(router).then((routerType) => {
-          if (routerType) {
-            verifExtencion(router).then((extMd)=>{
-              if(extMd){
-                fileRead(router).then((textArchivo) => {
-                  if (textArchivo) {
-                    detectarLinks()
-                  }
-                }) 
-              }
-            })
-          } else {
-            
-          }
-        
-        });
-      }
+//  const fileRead = (router) => {
+//    return new Promise((resolve, rejects) =>{
+//     fs.readFile(router, 'utf-8', (error, archivo) => {
+//       if (error) {
+//         rejects (error);
+//       }
+//       resolve(archivo); //extraer md
+//     });
     
-    } else {
-    
-    }
-  });
-}
+//    })
+  
+// };
+
+//   const verifExtencion = (router) => {
+//     return new Promise((resolve, rejects) => {
+//         const extencion= path.extname(router);
+//   if(extencion == '.md'){
+//     resolve(true)
+//   }else{
+//     rejects(error);
+//   }
+
+//     })
+
+// }
+// const detectarLinks = (arcchivo) => {
+//   const regexp =   '^https?:\/\/[\w\-]+(\.[\w\-]+)+[/#?]?.*$';
+//   return  archivo.matchAll(regexp);
+// }
+
+// const getLinks = (router) => {
+//   const readInterface = readline.createInterface({
+//       input: fs.createReadStream('/README.md'),
+//       output: process.stdout,
+//       console: false
+//   });
+
+// }
 
 
+// module.exports = (router) => {
+//   mdLinks(path);
+// };
 
-module.exports = (router) => {
-  mdLinks(path);
-};
+// //extraer links sin funcionar
+
+
+// var extractLinks = (array) => {
+//   console.log(array);
+//   let linkArray = [];
+//   linkArray.forEach((file) => {
+//     const readFiles = fs.readFileSync(file, 'utf-8');
+//     const renderer = new marked.Renderer();
+
+//     renderer.link = (href, __, text) => {
+//       linkArray.push({
+//         href: href,
+//         text: text.substring(0, 50),
+//         file: file
+//       });
+//     };
+
+//     marked(readFiles, {
+//       renderer: renderer
+//     });
+//   });
+//   return linkArray;
+// };
+
+//obtener url
+// var url = window.location.href
+// console.log(url)
+// }
